e15c3afbed88d2ded1dbacc8d0ab2126340ab04a
Wiktor Religo <wiciorny@gmail.com>
2021-12-07 11:37:01 +0100

    Initial commit, adding lengthUnit with test

---
+++ src/main/java/lengthData/LengthUnit.java
@@ -0,0 +1,8 @@
+package lengthData;
+
+import java.math.RoundingMode;
+
+interface LengthUnit {
+    int SCALE = 4;
+    RoundingMode ROUNDING_MODE = RoundingMode.CEILING;
+}
---
+++ src/test/java/LengthTest.java
@@ -0,0 +1,45 @@
+
+
+import org.junit.jupiter.api.Test;
+import lengthData.Centimeter;
+import lengthData.Inch;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+
+class LengthTests {
+
+    @Test
+    void shouldConvertZeroCentimetersValue() {
+        Centimeter cm = new Inch(BigDecimal.ZERO).toCentimeters();
+        assertEquals(BigDecimal.ZERO.setScale(4), cm.getValue());
+    }
+
+    @Test
+    void shouldConvertZeroInchesValue() {
+        Inch in = new Centimeter(BigDecimal.ZERO).toInches();
+        assertEquals(BigDecimal.ZERO.setScale(4), in.getValue());
+    }
+
+    @Test
+    void shouldConvertOneCentimeterValue() {
+        assertEquals(new BigDecimal("2.5400"), new Inch(BigDecimal.ONE).toCentimeters().getValue());
+    }
+
+    @Test
+    void shouldConvertOneInchValue() {
+        assertEquals(new BigDecimal("0.3938"), new Centimeter(BigDecimal.ONE).toInches().getValue());
+    }
+
+    @Test
+    void shouldntAcceptNegativeLengthInInches() {
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> new Inch(new BigDecimal(-1))
+        );
+        assertEquals("Length can't be negative!", exception.getMessage());
+    }
+}
