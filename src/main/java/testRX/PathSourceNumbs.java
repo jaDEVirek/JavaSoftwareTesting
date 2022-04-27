2cc55d914cba3f3457e8b18bd998c546edd6096f
Wiktor Religo <wiciorny@gmail.com>
2022-04-27 23:20:00 +0200

    hide data

--- src/main/java/testRX/PathSourceNumbs.java
+++ src/main/java/testRX/PathSourceNumbs.java
@@ -4,26 +4,30 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.stream.Stream;
 
 public class PathSourceNumbs {
     public static void main(String[] args) throws IOException {
 
-        System.out.println(Arrays.deepToString(turnFileIntoDataBase(Paths.get(
-                "D:\\Programistyczne\\IdeaProject\\JavaSoftwareTesting\\src\\main\\java\\testRX\\numerki.txt"))));
+        ArrayList<String> listaSlow = new ArrayList<>();
+        Arrays.stream(listaSlow.toArray()).limit(10).forEach(System.out::print);
+
+//        System.out.println(Arrays.deepToString(turnFileIntoDataBase(Paths.get(
+//                "D:\\Programistyczne\\IdeaProject\\JavaSoftwareTesting\\src\\main\\java\\testRX\\numerki.txt"))));
     }
 
-   static int[][] turnFileIntoDataBase(Path filePath) throws IOException {
-        //zalecam obsluge try with resources, dla zapewnienia gwarancji odczytu lub blok finally
-        return Files.lines(filePath)
-                .map((fileLine) -> fileLine.trim()
-                        .split(","))
-                .map((eachLineNumbers) -> Stream.of(eachLineNumbers)
-                        .mapToInt(Integer::parseInt)
-                        .toArray())
-                .toArray(int[][]::new);
-    }
+//   static int[][] turnFileIntoDataBase(Path filePath) throws IOException {
+//        //zalecam obsluge try with resources, dla zapewnienia gwarancji odczytu lub blok finally
+//        return Files.lines(filePath)
+//                .map((fileLine) -> fileLine.trim()
+//                        .split(","))
+//                .map((eachLineNumbers) -> Stream.of(eachLineNumbers)
+//                        .mapToInt(Integer::parseInt)
+//                        .toArray())
+//                .toArray(int[][]::new);
+//    }
 
 }
 
